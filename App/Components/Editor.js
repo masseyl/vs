8231e8adf29dfb8b91fed5d1b32053487c584743
masseyl <lmspam@neuropop.com>
2018-01-19 02:24:38 -0800

    add camera roll access

--- App/Components/Editor.js
+++ App/Components/Editor.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
-import { View } from 'react-native';
+import { View,  Modal, Text, TouchableOpacity, Image } from 'react-native';
 import { VideoPlayer, Trimmer } from 'react-native-video-processing';
 import ImagePicker from 'react-native-image-crop-picker';
 
@@ -12,15 +12,37 @@ export default class Editor extends Component {
     this.video = {
       currentTime: 0
     }
+    this.state = {
+      showModal: false,
+      source: ''
+    }
   }
 
   componentWillMount(){
+    this.openPicker()
+  }
+  openPicker = () => {
+    const attachments = [];
     ImagePicker.openPicker({
       mediaType: "video",
+      multiple: true,
+      includeBase64: true
     }).then((video) => {
-      console.log(video);
+      console.log(video)
+    })
+    .then(() => {
+      this.setState({
+        showModal: false
+      });
+      // TODO: "images" will need to be handled so we have something to upload
+      // this.props.addAttachments(attachments);
+    })
+    .catch((err) => {
+      this.setState({
+        cancelled: true
+      });
     });
-  }
+  };
 
   trimVideo = () => {
     const options = {
@@ -66,32 +88,52 @@ export default class Editor extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
-        <VideoPlayer
-          ref={ref => this.videoPlayerRef = ref}
-          startTime={30}  // seconds
-          endTime={120}   // seconds
-          play={true}     // default false
-          replay={true}   // should player play video again if it's ended
-          rotate={true}   // use this prop to rotate video if it captured in landscape mode iOS only
-          source={this.props.path}
-          playerWidth={300} // iOS only
-          playerHeight={500} // iOS only
-          style={{ backgroundColor: 'black' }}
-          resizeMode={VideoPlayer.Constants.resizeMode.CONTAIN}
-          onChange={({ nativeEvent }) => console.log({ nativeEvent })} // get Current time on every second
-        />
-        <Trimmer
-          source={this.props.path}
-          height={100}
-          width={300}
-          onTrackerMove={(e) => console.log(e.currentTime)} // iOS only
-          currentTime={this.video.currentTime} // use this prop to set tracker position iOS only
-          themeColor={'white'} // iOS only
-          thumbWidth={30} // iOS only
-          trackerColor={'green'} // iOS only
-          onChange={(e) => console.log(e.startTime, e.endTime)}
-        />
-      </View>
-    );
-  }
-}
+        <Modal
+          transparent
+          animationType="slide"
+          presentationStyle="overFullScreen"
+          visible={this.state.showModal}
+          >
+            <View style={styles.modalBackground}>
+              <View style={styles.modalContainer}>
+                <Text style={styles.modalCopy}>Add Photos From</Text>
+                <View style={styles.modalButtonContainer}>
+                  <TouchableOpacity
+                    style={styles.modalButton}
+                    onPress={this.openPicker}
+                    >
+                      <Text style={styles.modalButtonText}>Photo Library</Text>
+                    </TouchableOpacity>
+                    </View>
+                  </View>
+                  </View>
+                </Modal>
+                <VideoPlayer
+                  ref={ref => this.videoPlayerRef = ref}
+                  startTime={30}  // seconds
+                  endTime={120}   // seconds
+                  play={true}     // default false
+                  replay={true}   // should player play video again if it's ended
+                  rotate={true}   // use this prop to rotate video if it captured in landscape mode iOS only
+                  source={this.state.source}
+                  playerWidth={300} // iOS only
+                  playerHeight={500} // iOS only
+                  style={{ backgroundColor: 'white' }}
+                  resizeMode={VideoPlayer.Constants.resizeMode.CONTAIN}
+                  onChange={({ nativeEvent }) => console.log({ nativeEvent })} // get Current time on every second
+                />
+                <Trimmer
+                  source={this.state.source}
+                  height={100}
+                  width={300}
+                  onTrackerMove={(e) => console.log(e.currentTime)} // iOS only
+                  currentTime={this.video.currentTime} // use this prop to set tracker position iOS only
+                  themeColor={'white'} // iOS only
+                  thumbWidth={30} // iOS only
+                  trackerColor={'green'} // iOS only
+                  onChange={(e) => console.log(e.startTime, e.endTime)}
+                />
+              </View>
+            );
+          }
+        }
